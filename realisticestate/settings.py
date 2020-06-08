2602011d230a8773cb830ca4520390697327dfbc
jyoti <jshindems@gmail.com>
2020-06-08 01:09:20 -0400

    migrated changes and tested all routes for agent

--- realisticestate/settings.py
+++ realisticestate/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'users'
+    'users',
+    'agent',
 ]
 
 MIDDLEWARE = [
--- realisticestate/urls.py
+++ realisticestate/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('users.urls')),
+    path('api/agent/', include('agent.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
