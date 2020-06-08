744fdd243560e522a10a2977ac2c319e7e37ca68
jyoti <jyoti@jyotis-MacBook-Pro-2.local>
2020-06-08 00:44:50 -0400

    added views for agent

--- agent/views.py
+++ agent/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
+from .models import Agent
+from .serializers import AgentSerializer
 # Create your views here.
+
+class AgentListView(ListAPIView):
+    permission_classes =(permissions.AllowAny,)
+    queryset = Agent.objects.all()
+    serializer_class=AgentSerializer
+    pagination_class = None
+
+class AgentView(RetrieveAPIView):
+    queryset = Agent.objects.all()
+    serializer_class = AgentSerializer
+
+class TopSellerView(ListAPIView):
+    permission_classes =(permissions.AllowAny,)
+    queryset = Agent.objects.filter(top_seller=True)
+    serializer_class = AgentSerializer
+    pagination_class = None
+
